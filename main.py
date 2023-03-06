2fee500e7f7df0692fefc8c2b0d89adad5f16bd3
kris j <kris.jurkans@gmail.com>
2023-03-06 20:09:42 +0000

    added line to add files to list, TODO in parse.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 from pathlib import Path
 import machine
 from options_enum import Options
+import parse
 
 # py -3.10-64 main.py
 
@@ -14,17 +15,23 @@ def parse_options(machine_object):
             option_string = option[1:]
             match option_string:
                 case Options.OUTPUT_PREPROCESSED.value:
-                    # add to machine list
-                    # generate preprocessed thing
-                    machine_object.options.append(Options.OUTPUT_PREPROCESSED.name)
+                    machine_object.options.append(Options.OUTPUT_PREPROCESSED)
                 case Options.OUTPUT_SPIRV_ASM.value:
-                    machine_object.options.append(Options.OUTPUT_SPIRV_ASM.name)
+                    machine_object.options.append(Options.OUTPUT_SPIRV_ASM)
+                case Options.DEFINE_TOP_MODULE.value:
+                    machine_object.options.append(Options.DEFINE_TOP_MODULE)
+                    # TODO: how to find current pos in sys.argv and to look ahead one?
+                    # this must be the top module name
                 case _:
-                    raise Exception(f"unknown option '{option_string}', exiting.", "bad_option")
+                    raise Exception(f"unknown option '{option}', exiting.", "bad_option")
             
         elif option[-3:] == ".py":
-            print("got filename")
-            print(f"file exist? {Path(sys.argv[1]).is_file()}")
+            file_exists = Path(option).is_file()
+
+            if file_exists:
+                machine_object.files.append(option)
+            else:
+                print(f"file {option} does not exist.")
         else:
             raise Exception(f"unable to parse '{option}', exiting.", "parse_option_fail")
         
@@ -32,10 +39,11 @@ def parse_options(machine_object):
 
 def main():
 
-    print(sys.argv)
+    print(f"SYS ARG: {sys.argv}")
 
     machine_object = machine.Machine()
 
+    # argument parse call and error handle
     if len(sys.argv[1:]) == 0:
         print(f"got no arguments")
     else:
@@ -50,9 +58,14 @@ def main():
                 return -1
             
     # debug
+    print()
     print(machine_object.options)
     print(machine_object.files)
     print(machine_object.processed_text)
+    print()
+
+
+    parse.preprocess(machine_object)
 
 
 if __name__ == "__main__":
--- parse.py
+++ parse.py
@@ -4,5 +4,13 @@ import pyparsing as p
 # replace colons too
 # i.e function def x(): should become def x(){}
 
-def preprocess():
-    return None
+def preprocess(machine_object):
+
+    for file in machine_object.files:
+        print(f"FILE: {file}")
+
+        # setting up the grammar
+        # : --> {
+        # how to detect a change in indentation level?
+        #   after :, indentation++, so update some var?
+        # TODO: https://github.com/pyparsing/pyparsing/blob/master/examples/indentedGrammarExample.py
