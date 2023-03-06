0f04edae67f922404b6ea2b25d233ab5eb7b00cf
kris j <kris.jurkans@gmail.com>
2023-03-06 22:16:22 +0000

    preprocess plan

--- parse.py
+++ parse.py
@@ -14,3 +14,10 @@ def preprocess(machine_object):
         # how to detect a change in indentation level?
         #   after :, indentation++, so update some var?
         # TODO: https://github.com/pyparsing/pyparsing/blob/master/examples/indentedGrammarExample.py
+
+        # 1. parse functions + statements -- assign result names
+        # 2. search through parse results for : and skip over body
+        # 3. idea is that the function declaration, :, and function body will be seperate
+        #       allowing me to just copy the contents into another array or something
+        #
+        #   https://stackoverflow.com/questions/24954340/change-string-during-pyparsing
