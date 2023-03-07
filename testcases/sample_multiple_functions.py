2a9b2b9d49c922315c5661c889cad5fc09948619
kris j <kris.jurkans@gmail.com>
2023-03-07 00:18:44 +0000

    added multi-level indentation example

--- testcases/sample_multiple_functions.py
+++ testcases/sample_multiple_functions.py
@@ -1,2 +1,13 @@
 def _x1():
+    x = x + 1 / (2 ** 3)
+    b = 0
+    if (x == True):
+        print(b)
+
+    def a():
+        if (x == True):
+            print(b)
     return 1
+
+def x_3(a):
+    return a
