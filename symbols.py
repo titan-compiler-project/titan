2f7bf7be6d0e909acfca5e7eea43cfc5ea37d4e1
kris j <kris.jurkans@gmail.com>
2023-03-15 22:15:22 +0000

    added more classes, "add" function

--- symbols.py
+++ symbols.py
@@ -1,10 +1,34 @@
-# from __future__ import annotations
 from enum import Enum
 from typing import NamedTuple, TYPE_CHECKING
 
 # https://stackoverflow.com/questions/39740632/python-type-hinting-without-cyclic-imports
 if TYPE_CHECKING:
     from pyparsing import ParserElement
+
+class DataType(Enum):
+    NONE = 0
+    INTEGER = 1
+    FLOAT = 2
+    BOOLEAN = 3
+
+    
+class Operation(Enum):
+    VARIABLE_DECLARATION = 0
+    CONSTANT_DECLARATION = 1
+    FUNCTION_DECLARATION = 2
+    ASSIGNMENT = 3
+    ADD = 4
+    SUB = 5
+    MULT = 6
+    DIV = 7
+
+
+class Information(NamedTuple):
+    datatype: DataType
+    operation: Operation
+    line_no = int
+    line: ParserElement
+
 
 class SymbolTable():
 
@@ -15,19 +39,10 @@ class SymbolTable():
     def __init__(self):
         pass
 
+    # add entry
+    def add(self, expression, information: Information):
+        self.content.update({expression: information})
 
-class Types(Enum):
-    NONE = 0
-    INTEGER = 1
-    FLOAT = 2
-    BOOLEAN = 3
-    CONSTANT = 4
-
-    
-class Information(NamedTuple):
-    type: Types
-    line_no = int
-    line: ParserElement
-
-
-
+    # delete entry
+    # return using key
+    # if exists bool
