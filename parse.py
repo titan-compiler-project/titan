8c82ae466e949b7c7ded9a0f07db6ee70c512f36
kris j <kris.jurkans@gmail.com>
2023-03-16 12:12:25 +0000

    removed old code

--- parse.py
+++ parse.py
@@ -4,9 +4,6 @@ from grammar import *
 from typing import NamedTuple
 import pyparsing as pp
 
-def preprocess_add_semicolon_helper(token):
-    print(token)
-
 def preprocess(machine_object):
 
     # tokenize IDs, got from experimentation
@@ -86,57 +83,12 @@ def preprocess(machine_object):
 # https://docs.python.org/3/library/typing.html
 def parse_processed_python(machine_object: machine.Machine):
 
-    # keyword_def = pp.Keyword("def").suppress()
-    # keyword_def = grammar.TitanPythonGrammar.keyword_def#.suppress()
-    # keyword_return = pp.Keyword("return").suppress()
-    # keyword_None = pp.Keyword("None")
-    # function_name = pp.pyparsing_common.identifier
-    # variable_name = pp.pyparsing_common.identifier
-    # l_br, r_br = map(pp.Literal, "()")
-    # l_cbr, r_cbr = map(pp.Literal, "{}")
-    # colon = pp.Literal(":").suppress()
-    # number = pp.Word(pp.nums) + pp.Optional("." + pp.Word(pp.nums))
-
-    # function_parameter_list = pp.delimitedList(variable_name) | pp.empty
-    # function_return_list = pp.Group(pp.delimitedList(variable_name | number | keyword_None))
-    # function_call = function_name + l_br + function_parameter_list + r_br
-    # arithmetic_expression = pp.infix_notation(variable_name | number, [
-    #     ('-', 1, pp.OpAssoc.RIGHT, generate.test_parse_action),
-    #     (pp.one_of("* /"), 2, pp.OpAssoc.LEFT, generate.test_parse_action),
-    #     (pp.one_of("+ -"), 2, pp.OpAssoc.LEFT, generate.test_parse_action)
-    # ])
-
-    # arithmetic_expression = pp.infix_notation(variable_name | number, [
-    #     ('-', 1, pp.OpAssoc.RIGHT),
-    #     (pp.one_of("* /"), 2, pp.OpAssoc.LEFT),
-    #     (pp.one_of("+ -"), 2, pp.OpAssoc.LEFT)
-    # ])
-    
-    # statement = pp.Group(variable_name + "=" + arithmetic_expression | function_call) #.set_parse_action(generate.test_parse_action)
-
-    #checks for: "def function_name(param1, param2):"
-    # function_definition = keyword_def + function_name.set_results_name("function_name") + l_br.suppress() + function_parameter_list.set_results_name("function_param_list") + r_br.suppress() + colon
-
-    # function_body = pp.Group(pp.ZeroOrMore(statement)).set_results_name("function_statements") + pp.Optional(keyword_return  + function_return_list.set_results_name("function_returns"))
-
-    # module = pp.ZeroOrMore(
-    #     pp.Group(
-    #         function_definition + l_cbr.suppress() + function_body + r_cbr.suppress()
-    #         )
-    #     )
-    
-    # module = TitanPythonGrammar.module
-
-
     for entry in machine_object.processed_text:
         # print(entry)
         # parse_result = module.parse_string(entry)
         parse_result = TitanPythonGrammar.module.parse_string(entry)
         machine_object.parsed_modules.append(parse_result)
         parse_result.pprint()
-
-        # print()
-        # print()
 
         for result in parse_result:
             machine_object.functions.append(
