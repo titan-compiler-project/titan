c40f02622f4327f6a76427f4e27bcbddd5c6b440
kris j <kris.jurkans@gmail.com>
2023-03-19 18:25:22 +0000

    added auto() to enums

--- symbols.py
+++ symbols.py
@@ -1,4 +1,4 @@
-from enum import Enum
+from enum import Enum, auto
 from typing import NamedTuple, TYPE_CHECKING
 
 # https://stackoverflow.com/questions/39740632/python-type-hinting-without-cyclic-imports
@@ -13,14 +13,14 @@ class DataType(Enum):
 
     
 class Operation(Enum):
-    VARIABLE_DECLARATION = 0
-    CONSTANT_DECLARATION = 1
-    FUNCTION_DECLARATION = 2
-    ASSIGNMENT = 3
-    ADD = 4
-    SUB = 5
-    MULT = 6
-    DIV = 7
+    VARIABLE_DECLARATION = auto()
+    CONSTANT_DECLARATION = auto()
+    FUNCTION_DECLARATION = auto()
+    ASSIGNMENT = auto()
+    ADD = auto()
+    SUB = auto()
+    MULT = auto()
+    DIV = auto()
 
 
 class Information(NamedTuple):
