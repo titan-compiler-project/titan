a1734f9d042e0dae7e1e5a9e016817325f20b044
kris j <kris.jurkans@gmail.com>
2023-03-27 13:43:51 +0100

    removed non-primative types

--- type.py
+++ type.py
@@ -6,11 +6,11 @@ class DataType(Enum):
     INTEGER = auto()
     FLOAT = auto()
     BOOLEAN = auto()
-    CONSTANT = auto()
-    PTR_INT = auto()
-    PTR_FLOAT = auto()
-    PTR_BOOL = auto()
-    PTR_CONST = auto()
+    # CONSTANT = auto()
+    # PTR_INT = auto()
+    # PTR_FLOAT = auto()
+    # PTR_BOOL = auto()
+    # PTR_CONST = auto()
 
 class StorageType(Enum):
     IN = auto()
