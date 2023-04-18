acaeaeb862504c18e0d0b5591af6d2f96f4e91d2
kris j <kris.jurkans@gmail.com>
2023-04-18 19:32:03 +0100

    added test for parameter list

--- titan/tests/test_grammar.py
+++ titan/tests/test_grammar.py
@@ -6,5 +6,12 @@ import grammar as g
     "0", "1", "-1", "0.0", "1.0", "-1.0"
 ])
 def test_python_number(num_val):
+    g.TitanPythonGrammar.number.parse_string(num_val, parse_all=True)
+
     
-    g.TitanPythonGrammar.number.parse_string(num_val, parse_all=False)
+@pytest.mark.parametrize("param_str", [
+    "", "a", "_a", "a, b", "_a, _b", "a, b, _c", "a, _b, _c"
+])
+def test_param_list(param_str):
+    g.TitanPythonGrammar.function_parameter_list.parse_string(param_str, parse_all=True)
+
