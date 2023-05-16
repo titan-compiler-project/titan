a4ad79fad991f005f83df381feb94d56a046592a
kris j <kris.jurkans@gmail.com>
2023-05-16 16:33:50 +0100

    added class to handle conditional/ternary operator

--- titan/operators.py
+++ titan/operators.py
@@ -12,3 +12,13 @@ class BinaryOp(BaseOp):
     def __init__(self, tokens):
         self.operator = tokens[0][1]
         self.operands = [tokens[0][0], tokens[0][2]]
+
+
+class TernaryCondOp():
+    def __init__(self, tokens):
+        self.true_val = tokens[0]
+        self.false_val = tokens[2]
+        self.condition = tokens[1]
+
+    def __repr__(self):
+        return f"({self.__class__.__name__}: true_val: {self.true_val} , false_val: {self.false_val} , cond: {self.condition})"
