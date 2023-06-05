a8ea521c272746f4197d0e67b26d6994264f19a3
kris j <kris.jurkans@gmail.com>
2023-06-05 20:16:18 +0100

    (WIP) added defs in header, implemented some sections

--- titan/comms/TitanComms/TitanComms.cpp
+++ titan/comms/TitanComms/TitanComms.cpp
@@ -3,3 +3,37 @@
                      Titan-created function blocks via the Titan Compiler (or 
                      any SPI-connected device that implements its protocol).
 */
+#include "Arduino.h"
+#include "TitanComms.h"
+#include "SPI.h"
+
+enum TitanComms::instruction {
+    NOP = 0x0,
+    WRITE = 0x1,
+    READ  = 0x2,
+    GET_METADATA = 0xFF
+};
+
+struct TitanComms::int24 {
+    unsigned int data : 24;
+};
+
+TitanComms::TitanComms(int pin, SPISettings spi_settings){
+    _cs_pin = pin;
+    _spi_settings = spi_settings;
+}
+
+void TitanComms::begin(){
+    pinMode(_cs_pin, OUTPUT);
+    digitalWrite(_cs_pin, HIGH); // cs is active low, so setting to high means its disabled
+    SPI.begin();
+}
+
+void TitanComms::write(int24 address, long value){
+
+    // have 8b instruction + 24bit address + 32bit value
+    // how to split into nice 8 or 16 bit calls?
+
+    
+
+}
--- titan/comms/TitanComms/TitanComms.h
+++ titan/comms/TitanComms/TitanComms.h
@@ -7,7 +7,26 @@
 #define TitanComms_h
 
 #include "Arduino.h"
+#include "SPI.h"
 
+// gotta remember:
+// char - 1 byte/8 bits
+// int - 2 bytes/16 bits
+// long - 4 bytes/32 bits
 
+class TitanComms {
+    public:
+        TitanComms(int pin, SPISettings spi_settings); // pass cs & spi settings?
+        enum instruction : byte; // need data type otherwise it complains about forward declaration
+        struct int24;
+        void begin(); // handles hardware config
+        void write(int24 address, long value);
+        unsigned long read();
+    private:
+        int _cs_pin;
+        SPISettings _spi_settings;
+        byte nop_and_read8();
+        int nop_and_read16();
+};
 
 #endif
