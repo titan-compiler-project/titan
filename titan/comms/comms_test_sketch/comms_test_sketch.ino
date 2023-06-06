d4946125d35f1b67b50fd319d7e635e45d9d8a3b
kris j <kris.jurkans@gmail.com>
2023-06-06 03:27:23 +0100

    simple driver sketch

--- titan/comms/comms_test_sketch/comms_test_sketch.ino
+++ titan/comms/comms_test_sketch/comms_test_sketch.ino
@@ -1,9 +1,26 @@
 #include "TitanComms.h"
+#include "SPI.h"
+
+const int CS_PIN = 8;
+
+TitanComms comms(CS_PIN, SPISettings(8000000, LSBFIRST, SPI_MODE3));
 
 void setup() {
-    // do something
+    Serial.begin(9600);
+    comms.begin();
 }
 
 void loop() {
-    // do something
+    TitanComms::int24 x = {0};
+
+    // for (int i = 0; i < 5; i++) {
+    //     comms.write(x, 3);
+    //     x.data += 1;
+    //     delay(750);
+    // }
+
+    x.data = 24;
+    comms.write(x, 77);
+    delay(1000);
+
 }
