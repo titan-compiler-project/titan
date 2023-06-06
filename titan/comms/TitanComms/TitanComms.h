b0aed8ea166e9e006b243735c0d6b157f10f3c28
kris j <kris.jurkans@gmail.com>
2023-06-06 03:26:12 +0100

    defined int24 struct and instructions, modified names, added helper func def

--- titan/comms/TitanComms/TitanComms.h
+++ titan/comms/TitanComms/TitanComms.h
@@ -16,17 +16,28 @@
 
 class TitanComms {
     public:
-        TitanComms(int pin, SPISettings spi_settings); // pass cs & spi settings?
-        enum instruction : byte; // need data type otherwise it complains about forward declaration
-        struct int24;
+        TitanComms(int cs_pin, SPISettings spi_settings); // pass cs & spi settings?
+        // enum instruction : byte; // need data type otherwise it complains about forward declaration
+        
+        enum instruction {
+            NOP = 0x00,
+            WRITE = 0x01,
+            READ = 0x02,
+            GET_METADATA = 0xFF
+        };
+        
+        struct int24 {
+            unsigned int data : 24;
+        };
         void begin(); // handles hardware config
         void write(int24 address, long value);
         unsigned long read();
     private:
         int _cs_pin;
         SPISettings _spi_settings;
-        byte nop_and_read8();
-        int nop_and_read16();
+        byte _nop_and_read8();
+        int _nop_and_read16();
+        void _extract_byte_from_int(int24 data, int byte_index, byte* storage_byte);
 };
 
 #endif
