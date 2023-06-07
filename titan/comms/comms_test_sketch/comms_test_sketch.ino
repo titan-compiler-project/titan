080cfa9a0bb50657e96afd175172ac67fb27f9ce
kris j <kris.jurkans@gmail.com>
2023-06-07 20:53:43 +0100

    added more debug info

--- titan/comms/comms_test_sketch/comms_test_sketch.ino
+++ titan/comms/comms_test_sketch/comms_test_sketch.ino
@@ -15,6 +15,9 @@ const int SCLK = 4000000;
 
 TitanComms comms(CS_PIN, SPISettings(SCLK, MSBFIRST, SPI_MODE3));
 int wrong_counter = 0;
+int total_requests = 0;
+
+TitanComms::u_int24 x = {0};
 
 void setup() {
     Serial.begin(192000);
@@ -22,27 +25,20 @@ void setup() {
 }
 
 void loop() {
-    TitanComms::u_int24 x = {0};
 
-    // for (int i = 0; i < 5; i++) {
-    //     comms.write(x, 3);
-    //     x.data += 1;
-    //     delay(750);
-    // }
-
-    unsigned int rx;
+    u_int32_t rx;
     x.data = 0xAABBCC;
     comms.write(x, 0xDEADBEEF);
-    // delayMicroseconds(10);
     rx = comms.read(x);
-
-    DEBUG_PRINT_STR("got: "); DEBUG_PRINT_HEX(rx); DEBUG_PRINTLN();
 
     if (rx != 0x23232323)
         wrong_counter++;
 
-    DEBUG_PRINT_STR("wrong hit: "); DEBUG_PRINT_INT(wrong_counter); DEBUG_PRINTLN();
+    DEBUG_PRINT_STR("wrong hit: "); DEBUG_PRINT_INT(wrong_counter); 
+        DEBUG_PRINT_STR(" out of "); DEBUG_PRINT_INT(total_requests); DEBUG_PRINTLN();
 
-    delay(1500);
+    total_requests++;
+
+    delay(1000);
 
 }
