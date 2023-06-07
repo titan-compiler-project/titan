54bea0366367d909fcff0cd85d5ebaa65f0a5312
kris j <kris.jurkans@gmail.com>
2023-06-07 20:35:42 +0100

    added teensy and debug stuff

--- titan/comms/comms_test_sketch/comms_test_sketch.ino
+++ titan/comms/comms_test_sketch/comms_test_sketch.ino
@@ -1,17 +1,28 @@
+#define DEBUG
+
 #include "TitanComms.h"
+#include "TitanCommsDebug.h"
 #include "SPI.h"
 
-const int CS_PIN = 48;
+// mega
+// const int CS_PIN = 48;
 
-TitanComms comms(CS_PIN, SPISettings(8000000, MSBFIRST, SPI_MODE3));
+// teensy
+const int CS_PIN = 0;
+// const int CLOCK = 72000000;
+// 562600
+const int SCLK = 4000000;
+
+TitanComms comms(CS_PIN, SPISettings(SCLK, MSBFIRST, SPI_MODE3));
+int wrong_counter = 0;
 
 void setup() {
-    Serial.begin(9600);
+    Serial.begin(192000);
     comms.begin();
 }
 
 void loop() {
-    TitanComms::int24 x = {0};
+    TitanComms::u_int24 x = {0};
 
     // for (int i = 0; i < 5; i++) {
     //     comms.write(x, 3);
@@ -19,8 +30,19 @@ void loop() {
     //     delay(750);
     // }
 
+    unsigned int rx;
     x.data = 0xAABBCC;
     comms.write(x, 0xDEADBEEF);
-    delay(1000);
+    // delayMicroseconds(10);
+    rx = comms.read(x);
+
+    DEBUG_PRINT_STR("got: "); DEBUG_PRINT_HEX(rx); DEBUG_PRINTLN();
+
+    if (rx != 0x23232323)
+        wrong_counter++;
+
+    DEBUG_PRINT_STR("wrong hit: "); DEBUG_PRINT_INT(wrong_counter); DEBUG_PRINTLN();
+
+    delay(1500);
 
 }
