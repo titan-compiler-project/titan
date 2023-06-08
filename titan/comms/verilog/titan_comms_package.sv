f575aedba0beb1555e131049f46ecc8d39204e5c
kris j <kris.jurkans@gmail.com>
2023-06-08 03:55:17 +0100

    both enum syntaxes seem to work fine

--- titan/comms/verilog/titan_comms_package.sv
+++ titan/comms/verilog/titan_comms_package.sv
@@ -1,9 +1,13 @@
-package TitanComms
+package TitanComms;
 
-    typedef enum [7:0] { 
-        NOP = 0,
-        WRITE = 1,
-        READ = 2
-     } instructions;
+    // typedef enum bit [7:0] { 
+    //     NOP = 0,
+    //     WRITE = 1,
+    //     READ = 2
+    //  } instructions;
+
+    typedef enum int {
+        NOP = 0, WRITE = 1, READ = 2
+    } instructions;
 
 endpackage
