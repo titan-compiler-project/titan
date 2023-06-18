3768c2788209366b4f711a1d6ecbc314c4dde566
kris j <kris.jurkans@gmail.com>
2023-06-18 16:28:13 +0100

    updated header to add additional instructions and function defs

--- titan/comms/TitanComms/TitanComms.h
+++ titan/comms/TitanComms/TitanComms.h
@@ -18,6 +18,9 @@ class TitanComms {
             NOP = 0x00,
             WRITE = 0x01,
             READ = 0x02,
+            STREAM = 0x03,
+            TRANSFER = 0x04,
+            REPEAT = 0x05,
             GET_METADATA = 0xFF
         };
         
@@ -27,11 +30,13 @@ class TitanComms {
         void begin(); // handles hardware config
         void write(u_int24 address, u_int32_t value);
         u_int32_t read(u_int24 address);
+        void repeat();
     private:
         int _cs_pin;
         SPISettings _spi_settings;
         u_int8_t _nop_and_read8();
         u_int16_t _nop_and_read16();
+        void _repeat();
         void _extract_byte_from_int(u_int24 data, int byte_index, u_int8_t* storage_byte);
         void _chip_select();
         void _chip_deselect();
