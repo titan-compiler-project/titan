2148150fd800d3e6308938cccb65b1ef4c303390
kris j <kris.jurkans@gmail.com>
2023-06-18 16:28:57 +0100

    updated sketch for testing

--- titan/comms/comms_test_sketch/comms_test_sketch.ino
+++ titan/comms/comms_test_sketch/comms_test_sketch.ino
@@ -18,6 +18,7 @@ int wrong_counter = 0;
 int total_requests = 0;
 
 TitanComms::u_int24 x = {0};
+TitanComms::u_int24 address = {0};
 
 void setup() {
     Serial.begin(192000);
@@ -26,18 +27,40 @@ void setup() {
 
 void loop() {
 
-    u_int32_t rx;
-    x.data = 0xAABBCC;
-    comms.write(x, 0xDEADBEEF);
-    rx = comms.read(x);
+    // u_int32_t rx;
+    // x.data = 0xAABBCC;
+    // comms.write(x, 0xDEADBEEF);
+    // rx = comms.read(x);
 
-    if (rx != 0x23232323)
+    // if (rx != 0x23232323)
+    //     wrong_counter++;
+
+    // total_requests++;
+
+    // DEBUG_PRINT_STR("wrong hit: "); DEBUG_PRINT_INT(wrong_counter); 
+    //     DEBUG_PRINT_STR(" out of "); DEBUG_PRINT_INT(total_requests); DEBUG_PRINTLN();
+
+    u_int32_t rx;
+    address.data = 0;
+
+    comms.write(address, 1);
+
+    address.data = 1;
+    comms.write(address, 2);
+
+    address.data = 2;
+    rx = comms.read(address);
+
+    comms.repeat(); // should see data pointer reset to 0
+
+    if (rx != 3)
         wrong_counter++;
 
     total_requests++;
 
-    DEBUG_PRINT_STR("wrong hit: "); DEBUG_PRINT_INT(wrong_counter); 
-        DEBUG_PRINT_STR(" out of "); DEBUG_PRINT_INT(total_requests); DEBUG_PRINTLN();
+    DEBUG_PRINT_STR("got: ") + DEBUG_PRINT_HEX(rx) + DEBUG_PRINT_STR(" expected 3 (missed ");
+        DEBUG_PRINT_INT(wrong_counter); DEBUG_PRINT_STR(" out of "); DEBUG_PRINT_INT(total_requests);
+        DEBUG_PRINTLN_STR(")");
 
-    delay(1000);
+    delay(100);
 }
