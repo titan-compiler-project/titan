9db84f1b645a709269bda0d83e36bbe2803dbdf9
kris j <kris.jurkans@gmail.com>
2025-05-05 18:00:38 +0100

    add fixture for default node assembler

--- tests/fixtures/node.py
+++ tests/fixtures/node.py
@@ -17,3 +17,15 @@ def fixture_create_base_node_ctx() -> node.NodeContext:
         input_left=None, input_right=None, operation=Operation.NOP,
         data=[], is_comparison=False, array_id="", array_index_id=""
     )
+
+@pytest.fixture(name="default_node_assembler")
+def fixture_create_default_node_assembler() -> node.NodeAssembler:
+    """
+    Fixture: Create a node assembler for tests
+
+    Returns:
+        A simple ``NodeAssembler`` object containing a "default_module" available for use
+    """
+    assembler = node.NodeAssembler()
+    assembler.create_module("default_module")
+    return assembler
